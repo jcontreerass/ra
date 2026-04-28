ef926d3244dd6f8c1db5a98ed7e08140238e7a23
jcontreerass <jorgecm.2004@gmail.com>
2026-04-28 20:21:49 +0200

    Fixing

--- intento2/init.sh
+++ intento2/init.sh
@@ -22,7 +22,7 @@ influxd &
 sleep 5
 telegraf --config telegraf.conf &
 sleep 5
-grafana server --config ../grafana.ini --homepath ../logs/grafana cfg:default.paths.logs=../logs/grafana
+grafana server --config ../grafana.ini --homepath ../logs/grafana/grafana.log cfg:default.paths.logs=../logs/grafana/grafana.log
 
 
 for i in $(seq 1 "$NUM_INSTANCES"); do
