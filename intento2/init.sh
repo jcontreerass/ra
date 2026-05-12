a8eb386368f9c84ec1b2450636bfd94909b1bb1b
get-charlie <carlos.serr.dev@gmail.com>
2026-05-12 20:14:17 +0200

    Updated init.sh to start soa service

--- intento2/init.sh
+++ intento2/init.sh
@@ -4,24 +4,27 @@ NUM_INSTANCES=$1
 BROKER_IP=${2:-"localhost"}
 
 if [ -z "$NUM_INSTANCES" ]; then
-    echo "$0 num_instances"
-    exit 1
+    echo "Starting 2 isnstances..."
+    NUM_INSTANCES="2"
 fi
 
 pkill -f mosquitto
 pkill -f prox2mqtt.py
 pkill -f influxd
 pkill -f telegraf
+pkill -f api_sensores.py
 sleep 1
 
 trap "kill 0" EXIT
 
 mosquitto -c mosquitto.conf &
-sleep 5
+sleep 2
 influxd &
-sleep 5
+sleep 2
 telegraf --config telegraf.conf &
-sleep 5
+sleep 2
+uvicorn api_sensores:app --host 0.0.0.0 --port 8000 &
+sleep 2
 
 for i in $(seq 1 "$NUM_INSTANCES"); do
     PORT=$((8000 + i)) 
